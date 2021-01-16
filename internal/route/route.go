7e812bc85f43cced2e514e2392971b1afd28fa7b
Rijal Asepnugroho <rasepnugroho@waresix.com>
2021-01-16 23:48:23 +0700

    add internal library package

---
+++ internal/route/route.go
@@ -0,0 +1,13 @@
+package route
+
+import (
+	"database/sql"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+// GrpcRoute func
+func GrpcRoute(grpcServer *grpc.Server, db *sql.DB, log *logrus.Entry) {
+
+}
