113cdf775729d086a78ef697e2207d0ffe0a6a18
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2021-02-20 17:17:41 +0700

    [FEAT][#10]create receive return detail model file

---
+++ internal/model/receive_return_detail.go
@@ -0,0 +1,9 @@
+package model
+
+import "inventory-service/pb/inventories"
+
+// ReceiveReturnDetail struct
+type ReceiveReturnDetail struct {
+	Pb              inventories.ReceiveReturnDetail
+	PbReceiveReturn inventories.ReceiveReturn
+}
