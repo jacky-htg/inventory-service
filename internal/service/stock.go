c2e74de77959a5cf5dbb607d3db1c688f5d6eb5c
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2021-02-21 22:52:32 +0700

    [FEAT][#12] closing stock service

--- internal/service/stock.go
+++ internal/service/stock.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"context"
 	"database/sql"
+	"inventory-service/internal/model"
+	"inventory-service/pb/inventories"
 	"inventory-service/pb/users"
 )
 
@@ -11,4 +14,30 @@ type Stock struct {
 	UserClient   users.UserServiceClient
 	RegionClient users.RegionServiceClient
 	BranchClient users.BranchServiceClient
+}
+
+// Closing Stock
+func (u *Stock) Closing(ctx context.Context, in *inventories.ClosingStockRequest) (*inventories.MyBoolean, error) {
+	var stockModel model.Stock
+	var err error
+
+	ctx, err = getMetadata(ctx)
+	if err != nil {
+		return &inventories.MyBoolean{}, err
+	}
+
+	tx, err := u.Db.BeginTx(ctx, nil)
+	if err != nil {
+		return &inventories.MyBoolean{}, err
+	}
+
+	err = stockModel.Closing(ctx, tx)
+	if err != nil {
+		tx.Rollback()
+		return &inventories.MyBoolean{}, err
+	}
+
+	tx.Commit()
+
+	return &inventories.MyBoolean{Boolean: true}, err
 }
