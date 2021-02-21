6036074c2d80204a0c1cf03b374ad3928fee822b
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2021-02-21 22:45:56 +0700

    [FEAT][#12] closing stock model

--- internal/model/stock.go
+++ internal/model/stock.go
@@ -1,4 +1,29 @@
 package model
 
+import (
+	"context"
+	"database/sql"
+	"inventory-service/internal/pkg/app"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
 // Stock struct
 type Stock struct{}
+
+// Closing Stock
+func (u *Stock) Closing(ctx context.Context, tx *sql.Tx) error {
+	stmt, err := tx.PrepareContext(ctx, `CALL closing_stocks($1, 0, 0)`)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Prepare closing stock: %v", err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, ctx.Value(app.Ctx("companyID")).(string))
+	if err != nil {
+		return status.Errorf(codes.Internal, "Exec closing stock: %v", err)
+	}
+
+	return nil
+}
