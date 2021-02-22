a92437fca6ffe089988fd5f2aa43ed2adc34205e
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2021-02-22 12:35:27 +0700

    [FEAT][#13] stock info model

--- internal/model/stock.go
+++ internal/model/stock.go
@@ -114,3 +114,83 @@ func (u *Stock) List(ctx context.Context, db *sql.DB) error {
 
 	return nil
 }
+
+// Info Stock
+func (u *Stock) Info(ctx context.Context, db *sql.DB) error {
+	const productSelect string = `
+	products.id, products.company_id, 
+	brands.id, brands.code, brands.name,
+	product_categories.id, product_categories.name,
+	products.code, products.name, products.minimum_stock, 
+	products.created_at, products.created_by, products.updated_at, products.updated_by,
+	`
+
+	var stockQuery string = `stock (` + ctx.Value(app.Ctx("companyID")).(string) + `, products.id)`
+	if len(u.ListInput.GetBranchId()) > 0 {
+		stockQuery = `stock_branch (` + ctx.Value(app.Ctx("companyID")).(string) + `, ` + u.ListInput.GetBranchId() + `, products.id)`
+	}
+
+	query := `SELECT ` + productSelect + stockQuery + ` 
+		FROM products 
+		JOIN brands ON products.brand_id = brands.id AND products.company_id = brands.company_id
+		JOIN product_categories ON products.product_category_id = product_categories.id AND products.company_id = product_categories.company_id 
+	`
+	where := []string{"products.company_id = $1 AND products.id = $2"}
+
+	if len(where) > 0 {
+		query += ` WHERE ` + strings.Join(where, " AND ")
+	}
+
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Prepare statement Get stock info: %v", err)
+	}
+	defer stmt.Close()
+
+	var pbProduct inventories.Product
+	var companyID string
+	var createdAt, updatedAt time.Time
+	var pbBrand inventories.Brand
+	var pbProductCategory inventories.ProductCategory
+	var stock int32
+	err = stmt.QueryRowContext(ctx, ctx.Value(app.Ctx("companyID")).(string), u.InfoInput.ProductId).Scan(
+		&pbProduct.Id, &companyID,
+		&pbBrand.Id, &pbBrand.Code, &pbBrand.Name,
+		&pbProductCategory.Id, &pbProductCategory.Name,
+		&pbProduct.Code, &pbProduct.Name, &pbProduct.MinimunStock,
+		&createdAt, &pbProduct.CreatedBy, &updatedAt, &pbProduct.UpdatedBy,
+		&stock,
+	)
+
+	if err == sql.ErrNoRows {
+		return status.Errorf(codes.NotFound, "Query Raw get stock info: %v", err)
+	}
+
+	if err != nil {
+		return status.Errorf(codes.Internal, "Query Raw get stock info: %v", err)
+	}
+
+	if companyID != ctx.Value(app.Ctx("companyID")).(string) {
+		return status.Error(codes.Unauthenticated, "its not your company data")
+	}
+
+	pbProduct.Brand = &pbBrand
+	pbProduct.ProductCategory = &pbProductCategory
+
+	pbProduct.CreatedAt, err = ptypes.TimestampProto(createdAt)
+	if err != nil {
+		return status.Errorf(codes.Internal, "convert createdAt: %v", err)
+	}
+
+	pbProduct.UpdatedAt, err = ptypes.TimestampProto(updatedAt)
+	if err != nil {
+		return status.Errorf(codes.Internal, "convert updateddAt: %v", err)
+	}
+
+	u.StockInfo = inventories.StockInfo{
+		Product: &pbProduct,
+		Qty:     stock,
+	}
+
+	return nil
+}
